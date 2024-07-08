58a9e5786fc239ff43468f3493b48d0096477140
rossespley@gmail.com <rossespley@gmail.com>
2024-07-08 05:08:05 +0100

    set basic outline for email creation for email accounts for new hires

---
+++ src/emailapp/Email.java
@@ -0,0 +1,24 @@
+package emailapp;
+
+public class Email {
+
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    //constructor to receive the first name and last name
+
+    //ask for the department and return it
+
+    //generate a random password
+
+    //set the mailbox capacity
+
+    //set the alternate email
+
+    //change the password
+
+}
